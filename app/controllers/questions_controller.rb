b0bdcf58b4752328730f6ddb1f9d3587f26a31d7
Ilya Bekirov <i.bekirov@parts-soft.ru>
2021-01-26 02:52:31 +0300

    User can ask a questions and add tests for it

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,47 +1,47 @@
 class QuestionsController < ApplicationController
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :find_question, only: %i[show edit update destroy]
+
   def index
     @questions = Question.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
+    @question = Question.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @question = Question.new(question_params)
 
     if @question.save
-      redirect_to @question
+      redirect_to @question, notice: 'Your question successfully created.'
     else
       render :new
     end
   end
 
   def update
-    if question.update(question_params)
-      redirect_to question
+    if @question.update(question_params)
+      redirect_to @question
     else
       render :edit
     end
   end
 
   def destroy
-    question.destroy
+    @question.destroy
     redirect_to question_path
   end
 
   private
 
-  def question
-    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  def find_question
+    @question = Question.find(params[:id])
   end
-
-  helper_method :question
 
   def question_params
     params.require(:question).permit(:title, :body)
