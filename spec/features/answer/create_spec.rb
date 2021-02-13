c891dd55579b4d991b365b255343d2542d542eeb
Ilya Bekirov <i.bekirov@parts-soft.ru>
2021-02-13 18:34:24 +0300

    Added the ability to add files to answers with test

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -25,6 +25,15 @@ feature 'User can create answer' do
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'answer with attached files', js: true do
+      fill_in 'Body', with: 'Answer for question'
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   scenario 'Unauthenticated user tries to answer the question', js: true do
