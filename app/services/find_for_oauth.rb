df048b748a57697130c99d34d41c2eebf9e2c7fe
Ilya Bekirov <i.bekirov@parts-soft.ru>
2021-03-28 00:58:31 +0300

    Small refactored find for oauth class

--- app/services/find_for_oauth.rb
+++ app/services/find_for_oauth.rb
@@ -9,21 +9,24 @@ class FindForOauth
     authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
     return authorization.user if authorization
 
-    email = auth.info[:email]
-    user = User.find_by(email: email)
+    create_auth
+    @user
+  end
 
-    if user
-      user.confirm
-    else
-      password = Devise.friendly_token[0, 20]
-      User.transaction do
-        user = User.create!(email: email,
-                            password: password,
-                            password_confirmation: password,
-                            confirmed_at: Time.zone.now)
-        user.authorizations.create!(provider: auth.provider, uid: auth.uid)
-      end
+  private
+
+  def create_auth
+    email = auth.info[:email]
+    @user = User.find_by(email: email)
+    return @user.authorizations.create(provider: auth.provider, uid: auth.uid) if @user
+
+    password = Devise.friendly_token[0, 20]
+    User.transaction do
+      @user = User.create!(email: email,
+                          password: password,
+                          password_confirmation: password,
+                          confirmed_at: Time.zone.now)
+      @user.authorizations.create!(provider: auth.provider, uid: auth.uid)
     end
-    user
   end
 end
